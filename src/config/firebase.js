01284b93b952ed6c29b70155a53d8c5021177000
Jean-Charles Audebert <jean-charles.audebert@maif.fr>
2025-05-28 13:26:38 +0200

    secrets

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDG9Itw8PXI3YYuESPJCRInLhnIdMcu7-Q",
-  authDomain: "instrus-d1d04.firebaseapp.com",
-  projectId: "instrus-d1d04",
-  storageBucket: "instrus-d1d04.firebasestorage.app",
-  messagingSenderId: "897499351518",
-  appId: "1:897499351518:web:acf0839627546ad41df54f",
-  measurementId: "G-4863MKN00E"
+  apiKey: import.meta.env.VITE_FB_APIKEY,
+  authDomain: import.meta.env.VITE_FB_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECTID,
+  storageBucket: import.meta.env.VITE_FB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FB_APPID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENTID
 };
 
 // Initialize Firebase
