4cbb57ad2d450c8b17480bae9e3c820b040af5af
Jean-Charles Audebert <jean-charles.audebert@maif.fr>
2025-05-28 16:31:49 +0200

    Ajout prenom dans instrumentList

--- src/components/instruments/InstrumentList.jsx
+++ src/components/instruments/InstrumentList.jsx
@@ -1,27 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import Modal from 'react-modal';
 import ReactPaginate from "react-paginate";
-import { FaEdit, FaPlus, FaTrash, FaSortAlphaDown } from "react-icons/fa";
-import { useData } from "../../context/dataContext.jsx";
+import {FaEdit, FaPlus, FaTrash, FaSortAlphaDown} from "react-icons/fa";
+import {useData} from "../../context/dataContext.jsx";
 import {deleteInstrument, readAllInstruments} from "../../services/instrumentService.js";
 import InstrumentForm from "../form/InstrumentForm.jsx";
+import {readAllPlayers} from '../../services/playerService';
+import PlayerForm from '../form/PlayerForm';
 
 const initialInstrument = {
-    code: '',
-    hasLight: false,
-    isFree: true,
-    isSticked: true,
-    observation: '',
-    playerId: '',
-    pupitre: ''
+    code: '', hasLight: false, isFree: true, isSticked: true, observation: '', playerId: '', pupitre: ''
 };
 
-const InstrumentList = ({ searchTerm}) => {
-    const { instruments, setInstruments } = useData();
+const InstrumentList = ({searchTerm}) => {
+    const [players, setPlayers] = useState([]);
+    const {instruments, setInstruments} = useData();
     const [selectedInstrument, setSelectedInstrument] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isEditing, setIsEditing] = useState(false);
-    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+    const [sortConfig, setSortConfig] = useState({key: null, direction: 'ascending'});
     const [currentPage, setCurrentPage] = useState(0);
     const itemsPerPage = 10;
 
@@ -34,13 +31,19 @@ const InstrumentList = ({ searchTerm}) => {
         }
     };
 
-    useEffect(() => {
-        fetchInstruments()
-    }, []);
-
-    const handlePageClick = (data) => {
-        setCurrentPage(data.selected);
+    const fetchPlayers = async () => {
+        try {
+            const playersList = await readAllPlayers();
+            setPlayers(playersList);
+        } catch (error) {
+            console.error("Erreur lors du chargement des joueurs :", error);
+        }
     };
+
+    useEffect(() => {
+        fetchInstruments();
+        fetchPlayers();
+    }, []);
 
     const openModalForEdit = (instrument) => {
         setSelectedInstrument(instrument);
@@ -64,10 +67,6 @@ const InstrumentList = ({ searchTerm}) => {
         await fetchInstruments()
     };
 
-    const handleInstrumentChange = (newInstrumentData) => {
-        setSelectedInstrument(newInstrumentData);
-    };
-
     const handleDelete = async (id) => {
         if (window.confirm("Êtes-vous sûr de vouloir supprimer cet instrument?")) {
             try {
@@ -84,15 +83,19 @@ const InstrumentList = ({ searchTerm}) => {
         if (sortConfig.key === key && sortConfig.direction === 'ascending') {
             direction = 'descending';
         }
-        setSortConfig({ key, direction });
+        setSortConfig({key, direction});
     };
 
-    const sortedItems = () => {
-        if (!sortConfig.key) return instruments;
-
-        return [...instruments].sort((a, b) => {
-            let aValue = a[sortConfig.key];
-            let bValue = b[sortConfig.key];
+    const sortedInstruments = () => {
+        const sorted = [...instruments].sort((a, b) => {
+            let aValue, bValue;
+            if (sortConfig.key === 'firstname') {
+                aValue = players.find(p => p.id === a.playerId)?.firstname || '';
+                bValue = players.find(p => p.id === b.playerId)?.firstname || '';
+            } else {
+                aValue = a[sortConfig.key];
+                bValue = b[sortConfig.key];
+            }
 
             if (typeof aValue === 'boolean') {
                 aValue = aValue ? 1 : 0;
@@ -103,10 +106,17 @@ const InstrumentList = ({ searchTerm}) => {
             if (aValue > bValue) return sortConfig.direction === 'ascending' ? 1 : -1;
             return 0;
         });
+        return sorted;
     };
 
-    const filteredInstruments = sortedItems().filter(item =>
-    item.code.toLowerCase().includes(searchTerm.toLowerCase()));
+    const filteredInstruments = sortedInstruments().filter(instru => {
+        const player = players.find(p => p.id === instru.playerId);
+        const playerName = player?.firstname?.toLowerCase() || '';
+        return (
+            instru.code.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            playerName.includes(searchTerm.toLowerCase())
+        );
+    });
 
     const currentItems = filteredInstruments.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
 
@@ -115,32 +125,38 @@ const InstrumentList = ({ searchTerm}) => {
             <table>
                 <thead>
                 <tr>
-                    <th className="th-order" onClick={() => requestSort('code')}>Instrument <FaSortAlphaDown /></th>
-                    <th className="th-order" onClick={() => requestSort('hasLight')}>Lumière <FaSortAlphaDown /></th>
-                    <th className="th-order" onClick={() => requestSort('isFree')}>Disponible <FaSortAlphaDown /></th>
-                    <th className="th-order" onClick={() => requestSort('pupitre')}>Pupitre <FaSortAlphaDown /></th>
-                    <th className="th-order" onClick={() => requestSort('isSticked')}>Stické <FaSortAlphaDown /></th>
-                    <th className="th-order" onClick={() => requestSort('observation')}>Observation</th>
+                    <th onClick={() => requestSort('code')}>Instrument <FaSortAlphaDown /></th>
+                    <th onClick={() => requestSort('hasLight')}>Lumière <FaSortAlphaDown /></th>
+                    <th onClick={() => requestSort('isFree')}>Disponible <FaSortAlphaDown /></th>
+                    <th onClick={() => requestSort('pupitre')}>Pupitre <FaSortAlphaDown /></th>
+                    <th onClick={() => requestSort('isSticked')}>Stické <FaSortAlphaDown /></th>
+                    <th onClick={() => requestSort('observation')}>Observation</th>
+                    <th onClick={() => requestSort('firstname')}>Prénom <FaSortAlphaDown /></th>
                     <th>Actions <button className="create-icon" onClick={openModalForCreate}><FaPlus /></button></th>
                 </tr>
                 </thead>
                 <tbody>
-                {currentItems.map(instrument => (
-                    <tr key={instrument.id}>
-                        <td>{instrument.code}</td>
-                        <td>{instrument.hasLight ? 'Oui' : 'Non'}</td>
-                        <td>{instrument.isFree ? 'Oui' : 'Non'}</td>
-                        <td>{instrument.pupitre}</td>
-                        <td>{instrument.isSticked ? 'Oui' : 'Non'}</td>
-                        <td>{instrument.observation}</td>
-                        <td>
-                            <button className="edit-icon" onClick={() => openModalForEdit(instrument)}><FaEdit /></button>
-                            <button className="trash-icon" onClick={() => handleDelete(instrument.id)}><FaTrash /></button>
-                        </td>
-                    </tr>
-                ))}
+                {currentItems.map(instru => {
+                    const player = players.find(p => p.id === instru.playerId);
+                    return (
+                        <tr key={instru.id}>
+                            <td>{instru.code}</td>
+                            <td>{instru.hasLight ? 'Oui' : 'Non'}</td>
+                            <td>{instru.isFree ? 'Oui' : 'Non'}</td>
+                            <td>{instru.pupitre}</td>
+                            <td>{instru.isSticked ? 'Oui' : 'Non'}</td>
+                            <td>{instru.observation}</td>
+                            <td>{player ? player.firstname : 'Aucun'}</td>
+                            <td>
+                                <button className="edit-icon" onClick={() => openModalForEdit(instru)}><FaEdit /></button>
+                                <button className="trash-icon" onClick={() => handleDelete(instru.id)}><FaTrash /></button>
+                            </td>
+                        </tr>
+                    );
+                })}
                 </tbody>
             </table>
+
             <ReactPaginate
                 previousLabel={'previous'}
                 nextLabel={'next'}
@@ -148,10 +164,11 @@ const InstrumentList = ({ searchTerm}) => {
                 pageCount={Math.ceil(filteredInstruments.length / itemsPerPage)}
                 marginPagesDisplayed={2}
                 pageRangeDisplayed={5}
-                onPageChange={handlePageClick}
+                onPageChange={(e) => setCurrentPage(e.selected)}
                 containerClassName={'pagination'}
                 activeClassName={'active'}
             />
+
             <Modal
                 isOpen={isModalOpen}
                 onRequestClose={closeModal}
@@ -160,7 +177,7 @@ const InstrumentList = ({ searchTerm}) => {
                 <h2>{selectedInstrument ? 'Modifier un instrument' : 'Créer un instrument'}</h2>
                 <InstrumentForm
                     instrument={selectedInstrument || initialInstrument}
-                    onChange={handleInstrumentChange}
+                    onChange={setSelectedInstrument}
                     onSubmit={handleFormSubmit}
                     onCancel={closeModal}
                     submitLabel={isEditing ? 'Mettre à jour' : 'Créer'}
